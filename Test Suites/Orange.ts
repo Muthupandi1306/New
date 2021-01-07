3f4fdc6e44a3082aa25de969b7826496ee632b18
Muthupandi1306 <muthu13pandi@gmail.com>
2021-01-07 12:30:29 +0530

    Orange HRM Sample

---
+++ Test Suites/Orange.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Orange</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>32c535c4-4411-46e7-8c5c-f43b35972274</testSuiteGuid>
+   <testCaseLink>
+      <guid>279bdede-19b3-4387-bff5-0294b2182d57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>90540f6c-2741-41d6-922c-73eb0b2cc600</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data/LoginInfo</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>90540f6c-2741-41d6-922c-73eb0b2cc600</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>d22e1f1d-7bb7-409b-b12d-e5ebf5be2154</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>90540f6c-2741-41d6-922c-73eb0b2cc600</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>fb0ab821-0af0-4eb6-89ca-7b0cf33a4e8e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
